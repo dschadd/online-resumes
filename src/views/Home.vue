12be6396f01818392318c14c9558f49fe0c201ac
David Dimperio <daviddimperio@Davids-MacBook-Pro.local>
2018-12-03 19:03:24 -0600

    david change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" /> <HelloWorld msg="Welcome to Your Vue.js App" />
     <p>What up</p>
     <h1>daniel change</h1>
+    <h1>david change</h1>
   </div>
 </template>
 
