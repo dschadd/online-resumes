5ec41d6135167ef5b2c3ca34523a74f227f9cb2e
David Dimperio <daviddimperio@Davids-MacBook-Pro.local>
2018-12-04 19:48:17 -0600

    Add Education

--- src/views/ResumeShow.vue
+++ src/views/ResumeShow.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="resume-show">
-    <h1>{{ student }}</h1>
+    <!-- <h1>{{ student }}</h1> -->
+    <div class="container">
+      <h2>Education:</h2>
+      <div v-for="education in student.education" class="card border-secondary mb-3">
+        <div class="card-body">
+          <h5 class="card-title">{{ education.university_name }}</h5>
+          <p class="card-text">{{ education.degree }}</p>
+          <p class="card-text">{{ education.details }}</p>
+          <p class="card-text">{{ education.start_date + "-" + education.end_date}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,9 +27,12 @@ export default {
     };
   },
   created: function() {
-    axios.get("http://localhost3000/api/students/" + this.$route.params.id).then(function(response) {
-      this.student = response.data;
-    });
+    axios.get("https://gentle-sierra-69054.herokuapp.com/api/students/1").then(
+      function(response) {
+        console.log(response.data);
+        this.student = response.data;
+      }.bind(this)
+    );
   },
   methods: {},
   computed: {}
