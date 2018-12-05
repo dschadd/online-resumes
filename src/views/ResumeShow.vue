16723a0b9824cb10a6af4d33b2884d2ca6ceb3a4
darinnelson <dnelson5614@gmail.com>
2018-12-04 20:38:45 -0600

    Experiences, Capstone, Skills

--- src/views/ResumeShow.vue
+++ src/views/ResumeShow.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="resume-show">
-    <h1>{{ student }}</h1>
+
+  <div class="resume-show" align="left" style="width:960px">
+    <h1><strong>Experience</strong></h1>
+    <hr>
+    <div v-for="experience in student.experience" align="left">
+      <h6 style="text-align:left"><strong>{{ experience.company_name }}, {{ experience.job_title }}
+        <span style="float:right">{{experience.start_date}} - {{experience.end_date}}</span></strong></h6>
+      <h6>{{ experience.details }}</h6>
+      <br>
+    </div>
+    <br>
+    <h1><strong>Capstone</strong></h1>
+    <hr>
+    <div v-for="capstone in student.capstone" align="left">
+      <h6>{{ capstone.name }}</h6>
+      <h6>{{ capstone.description }}</h6>
+      <h6>Website: {{ capstone.url }}</h6>
+      <h6>{{ capstone.screenshot }}</h6>
+      <br>
+    </div>
+    <br>
+    <h1><strong>Skills</strong></h1>
+    <hr>
+    <span v-for="(skill, index) in student.skills">
+      <span>{{skill.skill_name}}</span><span v-if="index+1 < student.skills.length">, </span>
+    </span>
   </div>
 </template>
 
-<style></style>
+<style>
+
+</style>
 
 <script>
 var axios = require("axios");
@@ -12,13 +38,13 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      student: {}
+      student: {},
     };
   },
   created: function() {
-    axios.get("http://localhost3000/api/students/" + this.$route.params.id).then(function(response) {
+    axios.get("https://gentle-sierra-69054.herokuapp.com/api/students/1").then(function(response) {
       this.student = response.data;
-    });
+    }.bind(this));
   },
   methods: {},
   computed: {}
