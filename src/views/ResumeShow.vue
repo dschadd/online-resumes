60cdc79b0c91f048feedf1d755f6d4c5d42f0582
dschadd <dschadd@gmail.com>
2018-12-04 19:52:44 -0600

    finish student info card

--- src/views/ResumeShow.vue
+++ src/views/ResumeShow.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="resume-show">
-    <h1>{{ student }}</h1>
+  <div class="container">
+    <img class="card-img-top" src="" alt="Profile pic" />
+    <div class="card text-center">
+      <div class="card-body">
+        <h4 class="card-title">{{ student.first_name + " " + student.last_name }}</h4>
+        <p class="card-text">{{ student.short_bio }}</p>
+        <a :href="student.linkedin_url" class="btn btn-primary ml-2" target="_blank">LinkedIn</a>
+        <a :href="student.website_url" class="btn btn-primary ml-2" target="_blank">Personal Website</a>
+        <a :href="student.github_url" class="btn btn-primary ml-2" target="_blank">Github</a>
+      </div>
+      <div class="card-footer text-muted">{{ student.email + " " + student.phone_number }}</div>
+    </div>
   </div>
 </template>
 
@@ -16,9 +26,11 @@ export default {
     };
   },
   created: function() {
-    axios.get("http://localhost3000/api/students/" + this.$route.params.id).then(function(response) {
-      this.student = response.data;
-    });
+    axios.get("https://gentle-sierra-69054.herokuapp.com/api/students/1").then(
+      function(response) {
+        this.student = response.data;
+      }.bind(this)
+    );
   },
   methods: {},
   computed: {}
