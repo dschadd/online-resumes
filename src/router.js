04a4e4fa91374c0ee3b10c2ed03377628baf6f08
dschadd <dschadd@gmail.com>
2018-12-04 18:19:55 -0600

    add show and index pages

--- src/router.js
+++ src/router.js
@@ -1,23 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import ResumeShow from "./views/ResumeShow.vue";
+import StudentIndex from "./views/StudentIndex.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: "/", name: "home", component: Home },
+    { path: "/resume", name: "resume-show", component: ResumeShow },
+    { path: "/students", name: "student-index", component: StudentIndex }
   ]
-})
+});
---
+++ src/views/ResumeShow.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="resume-show">
+    <h1>{{ student }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+var axios = require("axios");
+
+export default {
+  data: function() {
+    return {
+      student: {}
+    };
+  },
+  created: function() {
+    axios.get("http://localhost3000/api/students/" + this.$route.params.id).then(function(response) {
+      this.student = response.data;
+    });
+  },
+  methods: {},
+  computed: {}
+};
+</script>
